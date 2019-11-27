015e65531bd6eed260c9a333179f6e11914499c1
Matteo Palladino <pastnullinfinity@gmail.com>
2019-11-27 16:21:22 +0100

    Rewrite of dynamodb fixture

--- tests/acl/test_acl_database.py
+++ tests/acl/test_acl_database.py
@@ -1,63 +1,38 @@
 import os
 
-import pytest
 import boto3
-from botocore.stub import Stubber, ANY
+import pytest
+from botocore.stub import ANY, Stubber
 
+import bless.acl.acl_database as test
 from bless.acl import acl_database
+from tests.acl import dynamodb_vectors as vector
 
-dynamodb_response = {
-    "ConsumedCapacity": {
-        "CapacityUnits": 1,
-        "GlobalSecondaryIndexes": {
-            "string": {
-                "CapacityUnits": 1,
-                "ReadCapacityUnits": 1,
-                "WriteCapacityUnits": 1
-            }
-        },
-        "LocalSecondaryIndexes": {
-            "string": {
-                "CapacityUnits": 1,
-                "ReadCapacityUnits": 1,
-                "WriteCapacityUnits": 1
-            }
-        },
-        "ReadCapacityUnits": 1,
-        "Table": {
-            "CapacityUnits": 1,
-            "ReadCapacityUnits": 1,
-            "WriteCapacityUnits": 1
-        },
-        "TableName": "bless-acl-table",
-        "WriteCapacityUnits": 1
-    },
-    "Item": {
-        "string": {
-            "SS": ["test"]  # Put here the values of the stubbed dynamoDB call
-        }
-    }
-}
-
-expected_params = {"Key": ANY,"TableName": ANY}
+expected_params = {"Key": ANY, "TableName": ANY}
 
 
 def test_mock_dynamodb():
     dynamodb = boto3.resource("dynamodb")
     stubber = Stubber(dynamodb.meta.client)
-    stubber.add_response('get_item', dynamodb_response, expected_params)
+    stubber.add_response('get_item', vector.dynamodb_response_generic, expected_params)
 
     with stubber:
         service_response = dynamodb.Table("bless-acl-table").get_item(Key={"GroupId": "group_id"})
 
-    assert service_response == dynamodb_response
-
+    assert service_response == vector.dynamodb_response_generic
 
 
 @pytest.fixture
-def dynamodb_resource():
-    import boto3
-    from botocore.stub import Stubber, ANY
-    dynamodb = boto3.resource("dynamodb")
-    stubber = Stubber(dynamodb.meta.client)
-    return stubber
+def dynamodb_resource_stub(autouse=True):
+    with Stubber(test.dynamodb_resource.meta.client) as stubber:
+        yield stubber
+        stubber.assert_no_pending_responses()
+
+
+def test_single_username(dynamodb_resource_stub):
+    dynamodb_resource_stub.add_response(
+        'get_item',
+        vector.dynamodb_response_generic,
+        expected_params={'Key': ANY}
+    )
+    result = test.get_valid_iam_principals('bogus-iam-user','bogus-acl-arn')
